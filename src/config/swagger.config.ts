d5439fdc24ea30ad446d7de2548eb6be1aed555c
Szymon <szymon.bartczak@rst-it.com>
2019-11-13 14:59:40 +0100

    Used yarn instead of npm, fixed lint error, changed readme a bit

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,6 +1,6 @@
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-export default function(app){
+export default function(app) {
     const options = new DocumentBuilder()
     .setTitle(process.env.APP_NAME)
     .setDescription(process.env.APP_DESC)
